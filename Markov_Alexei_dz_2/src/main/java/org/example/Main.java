084a39f9ab43d37fd84432f18c754a9789d8c58c
Alex Mark <alexvmar@yandex.ru>
2023-06-16 07:42:56 +0300

    change files homework2

--- Markov_Alexei_dz_2/src/main/java/org/example/Main.java
+++ Markov_Alexei_dz_2/src/main/java/org/example/Main.java
@@ -1,7 +1,58 @@
-package org.example;
+//Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса,
+// используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
+//Если значение null, то параметр не должен попадать в запрос.
+//Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+//В итоге должно получится select * from students where name=Ivanov, country=Russia, city=Moscow
+//
 
+
+//Дополнительные задания
+//
+//Дана json-строка (можно сохранить в файл и читать из файла)
+//[{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4",
+// "предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+//Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида:
+// Студент [фамилия] получил [оценка] по предмету [предмет].
+//Пример вывода:
+//Студент Иванов получил 5 по предмету Математика.
+//Студент Петрова получил 4 по предмету Информатика.
+//Студент Краснов получил 5 по предмету Физика.
+//
+//*Сравнить время выполнения замены символа "а" на "А" любой строки содержащей >1000 символов средствами String
+// и StringBuilder.
+
+
+package org.example;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+            String [] list = lib.ReadLineFromFile("file.txt");
+            System.out.println(list[0]);
+            StringBuilder resultSelect = LineInList(list[0]);
+            System.out.println(resultSelect);
+        }
+    public static StringBuilder LineInList(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        System.out.println(line3);
+        StringBuilder result = new StringBuilder("select * from students where ");
+
+        String [] arrayData = line3.split(", ");
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            if(arrData[1].equals("null") == false) {
+                if (i != 0) {
+                    result.append(", ");
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                } else {
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                }
+            }
+         }
+        return result;
     }
 }
