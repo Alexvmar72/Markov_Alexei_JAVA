06816920234ea9f3e22f6c8ce2beb7d95366812e
Alex Mark <alexvmar@yandex.ru>
2023-06-23 11:23:50 +0300

    change files homework4

--- Markov_Alexei_dz_4/task_4/src/main/java/org/example/Main.java
+++ Markov_Alexei_dz_4/task_4/src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -22,12 +22,12 @@ public class Main {
             System.out.printf("Введите пол: ");
             String sex = scanner.nextLine();
             resultUser
-                    .append("{\"ФИО\":\"")
+                    .append("[")
                     .append(name)
-                    .append("\",\"Возраст\":\"")
+                    .append("][")
                     .append(age)
-                    .append("\",\"Пол\":\"")
-                    .append(sex + "\"}" + "\n");
+                    .append("][")
+                    .append(sex + "]");
 
             all_users.add(String.valueOf(resultUser));
             System.out.print("Для ввода нового пользователя введите д: ");
@@ -41,12 +41,98 @@ public class Main {
     }
     public static void SaveInFile(List<String> array_save) throws Exception {
         FileWriter fileWriter = new FileWriter("user.txt", true);
-        fileWriter.write(array_save.toString() + "\n");
+        fileWriter.write(array_save.toString());
+        fileWriter.append('\n');
         fileWriter.close();
 
     }
 
+    public static void ReadFile() throws Exception {
+        File file_txt = new File("user.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file_txt));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            String[] array = line
+                    .replace("[[", "")
+                    .replace("]]", "")
+                    .replace("][", ",")
+                    .split("], \\[");
+
+            for (int i = 0; i < array.length; i++) {
+                String[] list = array[i]
+                        .replace("\"", "")
+                        .split(",");
+
+                StringBuilder resultSelect = new StringBuilder();
+                resultSelect
+                        .append("ФИО: ")
+                        .append(list[0])
+                        .append(" возраст: ")
+                        .append(list[1])
+                        .append(" пол: ")
+                        .append(list[2]);
+
+                System.out.println(resultSelect);
+            }
+        }
+    }
+    public static int CountSizeArray() throws Exception {
+        File file_txt = new File("user.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file_txt));
+        int count = 0;
+        String countUser;
+
+        while ((countUser = br.readLine()) != null ) {
+            String[] array = countUser
+                    .split(",");
+            count+= array.length;
+        }
+        return count;
+    }
+    public static void CreateSortArray(int sizeArray, int sortIndex) throws Exception {
+        File file_txt = new File("user.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file_txt));
+        String line;
+        String[][] BuildArray = new String[sizeArray][3];
+        int n = 0;
+        while ((line = br.readLine()) != null) {
+            String[] array = line
+                    .replace("[[", "")
+                    .replace("]]", "")
+                    .replace("][", ",")
+                    .split("], \\[");
+            //System.out.println(array);
+
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i]);
+                String[] list = array[i]
+                        .replace("\"", "")
+                        .split(",");
+                BuildArray[n] = list;
+                n++;
+            }
+        }
+        System.out.println();
+        System.out.println("Отсортированный список:");
+        Arrays.sort(BuildArray, Comparator.comparing(o -> o[sortIndex]));
+        for (int i = 0; i < BuildArray.length; i++)
+        {
+            for (int j = 0; (BuildArray[i] != null && j < BuildArray[i].length); j++) {
+                System.out.print(BuildArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //System.out.println(Arrays.deepToString(BuildArray));
+    }
+
+
     public static void main(String[] args) throws Exception {
-        SaveInFile(Array_Users());
+        //SaveInFile(Array_Users());
+        //ReadFile();
+        System.out.println("Размер будущего массива: " + CountSizeArray());
+        CreateSortArray(CountSizeArray(), 1);
+
     }
 }
