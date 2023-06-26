592fb1c3b7c66602ee97a62c467e05c4290f0c93
Alex Mark <alexvmar@yandex.ru>
2023-06-26 21:01:10 +0300

    created files homework5

--- Markov_Alexei_dz_5/task_5/src/main/java/org/example/Main.java
+++ Markov_Alexei_dz_5/task_5/src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        CreatedAbonent(map, "Алексей", "234");
+        CreatedAbonent(map, "Владимир", "345");
+        CreatedAbonent(map, "Илья", "456");
+        CreatedAbonent(map, "Светлана", "567");
+        CreatedAbonent(map, "Анна", "678");
+        CreatedAbonent(map, "Дарья", "789");
+        CreatedAbonent(map, "Владимир", "123");
+        System.out.println(map);
+
+        ArrayList<String> list_sort = new ArrayList<>();
+        for (String elem_sort: map.keySet()
+             ) {
+            list_sort.add(elem_sort);
+        }
+        Collections.sort(list_sort, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return map.get(o1).size() - map.get(o2).size();
+            }
+        });
+        System.out.println(list_sort);
     }
-}
+    private static void CreatedAbonent(HashMap<String, ArrayList<String>> map, String name, String phone) {
+        map.putIfAbsent(name, new ArrayList<>());
+        map.get(name).add(phone);
+    }
+} 
