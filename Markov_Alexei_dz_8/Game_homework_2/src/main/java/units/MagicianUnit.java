6152dd554df850a537fd3f81a32256123de83551
Alex Mark <alexvmar@yandex.ru>
2023-07-27 17:36:24 +0300

    change files homework12

--- Markov_Alexei_dz_8/Game_homework_2/src/main/java/units/MagicianUnit.java
+++ Markov_Alexei_dz_8/Game_homework_2/src/main/java/units/MagicianUnit.java
@@ -1,27 +1,59 @@
 package units;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Маг, волшебник
  * */
 public class MagicianUnit extends BaseUnit {
-    private int mana;
-    private int maxMana;
+    private int mana, attac;
 
     public MagicianUnit(String name, int x, int y) {
         super(12, 4, 3,  true, name, x, y, "ready", 1);
-        this.maxMana = this.mana = maxMana;
+        this.mana = 50;
+        this.attac = 1;
     }
 
     @Override
     public void step(ArrayList<BaseUnit> units, ArrayList<BaseUnit> units2) {
-        BaseUnit tmp = nearest(units);
-        System.out.println(tmp.name + " " + coordinates.countDistanse(tmp.coordinates));
+        if (!isAlive) return;
+        BaseUnit tmp = nearest(units2);
+        if ((int) coordinates.countDistanse(tmp.coordinates) <= attac) {
+            if (mana > 0) {
+                tmp.getDamage(damage);
+                mana -= 1;
+                state = "Attack";
+            } else {
+                mana += 1;
+                state = "Busy";
+            }
+        } else {
+            move(tmp.coordinates, units);
+            state = "Moving";
+        }
+        //super.step(units, units2);
+        ArrayList<BaseUnit> deadTeammates = new ArrayList<>();
+        //BaseUnit tmpAlly = units.get(0);
+        if (!isAlive) return;
+        for (BaseUnit unit: units) {
+            if (!unit.isAlive) {
+                deadTeammates.add(unit);
+            }
+        }
+        if (deadTeammates.size() > units.size() / 2 - 1 && mana >= 5) {
+            int rand = new Random().nextInt(deadTeammates.size() - 1);
+
+            deadTeammates.get(rand).isAlive = true;
+            deadTeammates.get(rand).hp = deadTeammates.get(rand).maxHp / 2;
+            state = "Revive";
+            mana = 0;
+            return;
+        }
     }
 
     @Override
     public String getInfo() {
-        return "Маг " + String.format("%s x: %d y: %d", name, coordinates.x, coordinates.y);
+        return "Маг " + String.format("%s x: %d y: %d mana: %d", name, coordinates.x, coordinates.y, mana);
     }
 }
--- Markov_Alexei_dz_8/Game_homework_2/src/main/java/units/MonkUnit.java
+++ Markov_Alexei_dz_8/Game_homework_2/src/main/java/units/MonkUnit.java
@@ -6,36 +6,28 @@ import java.util.ArrayList;
  * Монах
  * */
 public class MonkUnit extends units.BaseUnit {
-    protected int mana, magicDamage, attac, actionPriority;
+    protected int mana;
     public MonkUnit(String name, int x, int y){
         super(12, 4, 3,  true, name, x, y, "ready", 1);
         this.mana = 5;
-        this.magicDamage = 4;
-        this.attac = 1;
-        this.actionPriority = 2;
     }
 
     @Override
     public void step(ArrayList<BaseUnit> units, ArrayList<BaseUnit> units2) {
-        BaseUnit tmp = nearest(units2);
-        if (isAlive) {
-            if (state == "Busy") {
-                state = "Stand";
+        BaseUnit tmpAlly = units.get(0);
+        double minAllyHealth = 1;
+        if (!isAlive) return;
+        for (BaseUnit unit: units) {
+            if (unit.hp / unit.maxHp < minAllyHealth && unit.isAlive) {
+                minAllyHealth = unit.hp / unit.maxHp;
+                tmpAlly = unit;
             }
-        } else return;
-
-        if ((int) coordinates.countDistanse(tmp.coordinates) <= attac) {
-            if (mana > 0) {
-                tmp.getDamage(damage);
-                mana -= 1;
-                state = "Attack";
-            } else {
-                mana += 1;
-                state = "Busy";
-            }
-        } else {
-            move(tmp.coordinates, units);
-            state = "Moving";
+        }
+        if (minAllyHealth < 1 && mana > 0) {
+            tmpAlly.getDamage(-damage);
+            mana -= 1;
+            state = "Healing";
+            return;
         }
     }
 
